fe2a040a98d9d3bb7fc32cef80b75a69a8cd3bbe
chathudissa1931 <chathurangadissanayaka@yahoo.com>
2016-11-22 18:36:48 +0530

    added thread function

--- MS101VBN_A/Program.cs
+++ MS101VBN_A/Program.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 //pcap library including
 using PcapDotNet.Core;
+using PcapDotNet.Packets;
 
 namespace MS101VBN_A
 {
+    public class QoS
+    {
+        // This method that will be called when the thread is started
+        public void Speed()
+        {
+            while (true)
+            {
+                // Put the Main thread to sleep for 1 second
+                Thread.Sleep(1000);
+                Console.WriteLine("Alpha.Beta is running in its own thread.");
+            }
+        }
+    };
+
     class Program
     {
         static void Main(string[] args)
         {
+            QoS qos = new QoS();
+
             Console.WriteLine("Hellow world");
+            
+            // Create the thread object, passing in the Alpha.Beta method
+            // via a ThreadStart delegate. This does not start the thread.
+            Thread speedCalThrd = new Thread(new ThreadStart(qos.Speed));
+
+            // Start the thread
+            speedCalThrd.Start();
+            
+            Console.ReadKey();
+
+            // Request that oThread be stopped
+            //speedCalThrd.Abort();
 
             // Retrieve the device list from the local machine
             IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
@@ -23,7 +53,64 @@ namespace MS101VBN_A
             {
                 DevicePrint(allDevices[i]);
             }
+
+            //check the devices
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+                Console.Write((i + 1) + ". " + device.Name);
+                if (device.Description != null)
+                    Console.WriteLine(" (" + device.Description + ")");
+                else
+                    Console.WriteLine(" (No description available)");
+            }
+
+            int deviceIndex = 0;
+            do
+            {
+                Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
+                string deviceIndexString = Console.ReadLine();
+                if (!int.TryParse(deviceIndexString, out deviceIndex) ||
+                    deviceIndex < 1 || deviceIndex > allDevices.Count)
+                {
+                    deviceIndex = 0;
+                }
+            } while (deviceIndex == 0);
+            //test read value.
+            //Console.WriteLine(deviceIndex.ToString());
+
+            // Take the selected adapter
+            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+
+            // Open the device
+
+            // 65536                                    - portion of the packet to capture 65536 guarantees that the whole packet 
+            //                                             will be captured on all the link layers
+            // PacketDeviceOpenAttributes.Promiscuous   - promiscuous mode
+            // 1000                                     - read timeout
+            using (PacketCommunicator communicator = selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000))                                                    
+            {
+                Console.WriteLine("Listening on " + selectedDevice.Description + "...");
+
+                // start the capture
+                communicator.ReceivePackets(0, PacketHandler);
+            }
+
+
             Console.ReadKey();
+        }
+
+        // Callback function invoked by Pcap.Net for every incoming packet
+        private static void PacketHandler(Packet packet)
+        {
+            Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" + packet.Length);
         }
 
         // Print all the available information on the given interface
